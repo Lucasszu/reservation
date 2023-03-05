3464433a9da66a3bff7b3bb0292a1e2983c4817f
Lukasz-Szumski <lukasz.szumski@gmail.com>
2023-03-05 10:18:31 +0100

    basics

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const sum = require("./sum").sum;
+import { sum2 } from "./sum2";
+import { style } from "./css/index.scss";
+import Icon from "./assets/img/proba.png";
+
+console.log("Hello World");
+console.log(sum(2, 3));
+console.log(sum2(2, 3));
+
+let heading = document.querySelector("#demo"),
+	sumValue = sum(10, 5);
+
+heading.innerText = `10+10= ${sumValue}`;
+
+let myIcon = new Image();
+myIcon.src = Icon;
+
+document.querySelector("div").append(myIcon);
+document.querySelector("div").classList.add("change");
+
+fetch("destination.json")
+	.then(res => res.json()) // Transform the data into text
+	.then(data => {
+		console.log(data);
+	})
+	.catch(err => console.log(err));
